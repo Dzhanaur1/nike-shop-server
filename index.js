4c8fa3dc31b781665e038c1c38e70ef6e3181d40
Dzhanaur1 <3923328@gmail.com>
2023-07-12 16:34:55 +0300

    add cors

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ require("dotenv").config();
 const express = require("express");
 const sequelize = require("./db");
 const router = require("./routes");
-
+const cors = require("cors");
 const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 app.use("/api", router);
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*"); // разрешаем запросы с любого домена
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE"); // разрешаем методы GET, POST, PUT и DELETE
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization"); // разрешаем заголовки Content-Type и Authorization
-  next();
-});
+app.use(cors());
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*"); // разрешаем запросы с любого домена
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE"); // разрешаем методы GET, POST, PUT и DELETE
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization"); // разрешаем заголовки Content-Type и Authorization
+//   next();
+// });
 const start = async () => {
   try {
     await sequelize.authenticate();
